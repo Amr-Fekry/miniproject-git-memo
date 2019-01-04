ab84f28906f2858bdc589e0c5791ea427d71a80e
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-04 19:08:08 +0200

    Change CommandSection from QWidget to QFrame with a border

--- app.py
+++ app.py
@@ -11,22 +11,27 @@ class CommandPage(QtWidgets.QWidget):
 		self.setLayout(self.layout)
 		# add the label parameter to the page
 		self.command_label = QtWidgets.QLabel(label_parameter)
+		self.command_label.setObjectName("command_label")
 		self.command_label.setStyleSheet("""
-			QLabel {color:green; 
-			        font-size: 20px; 
-			        font-weight: bold; 
-			        margin: 20px; 
-			        padding: 5px;
-			        border: 1px solid black;
-			        border-radius: 2px
-			        }""")
+			#command_label {color:green; 
+			        		font-size: 20px; 
+			    		    font-weight: bold; 
+			   		        margin: 20px; 
+			    		    padding: 5px;
+			        		border: 1px solid black;
+			        		border-radius: 2px}""")
 		# add label to layout
 		self.layout.addWidget(self.command_label, 0, QtCore.Qt.AlignCenter) # align widget inside cell
 
-class CommandSection(QtWidgets.QWidget):
+class CommandSection(QtWidgets.QFrame):
 	def __init__(self, what_parameter, how_parameter):
-		# initialize CommandSection as a QWidget
+		# initialize CommandSection as a QFrame
 		super(CommandSection, self).__init__()
+		# add a border to the CommandSection QFrame
+		self.setObjectName("CommandSection")
+		self.setStyleSheet("""
+			#CommandSection {border: 1px solid lightgray;
+			                 border-radius: 5px}""")
 		# add a vertical layout
 		self.layout = QtWidgets.QVBoxLayout()
 		self.layout.setAlignment(QtCore.Qt.AlignTop)
@@ -36,16 +41,18 @@ class CommandSection(QtWidgets.QWidget):
 		self.what.setFixedWidth(500)
 		self.what.setWordWrap(True)
 		self.what.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed) # no expanding in x, y
-		self.what.setStyleSheet("QLabel {font-size: 14px}")
+		self.what.setObjectName("what")
+		self.what.setStyleSheet("#what {font-size: 14px}")
 		# add the how parameter to the section as a text area
 		self.how = QtWidgets.QLineEdit(how_parameter)
 		self.how.setReadOnly(True)
 		self.how.setFixedSize(500, 30)
+		self.how.setObjectName("how")
 		self.how.setStyleSheet("""
-			QLineEdit {font-size: 14px; 
-			           background-color: #5b7bad; 
-			           color: white; 
-			           padding-left: 5px}""")
+			#how {font-size: 14px; 
+			      background-color: #5b7bad; 
+			      color: white; 
+			      padding-left: 5px}""")
 		# add what and how to layout
 		self.layout.addWidget(self.what, 0, QtCore.Qt.AlignCenter)
 		self.layout.addWidget(self.how, 0, QtCore.Qt.AlignCenter)
@@ -64,10 +71,10 @@ class Window(QtWidgets.QMainWindow):
 		self.setCentralWidget(self.page1)
 		# test: adding a section to the page
 		self.sec1 = CommandSection("- Initializing a local git repository in the current directory", "git init")
-		self.page1.layout.addWidget(self.sec1)
+		self.page1.layout.addWidget(self.sec1, 0, QtCore.Qt.AlignCenter)
 
 		self.sec2 = CommandSection("- Add changes in the current working directory to the staging area before committing any changes.", "git add .")
-		self.page1.layout.addWidget(self.sec2)
+		self.page1.layout.addWidget(self.sec2, 0, QtCore.Qt.AlignCenter)
 
 
 	#________________________________________VIEWS_______________________________________
