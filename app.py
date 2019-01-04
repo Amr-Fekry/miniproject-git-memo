d3fa5384d11f3bae885bc604c3fb31b89f6775c0
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-04 17:50:02 +0200

    Edit alignments and style-sheets of widgets

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ class CommandPage(QtWidgets.QWidget):
 		super(CommandPage, self).__init__()
 		# add a vertical layout
 		self.layout = QtWidgets.QVBoxLayout()
-		self.layout.setAlignment(QtCore.Qt.AlignHCenter) # align cells of layout
+		self.layout.setAlignment(QtCore.Qt.AlignTop) # align cells of layout
 		self.setLayout(self.layout)
 		# add the label parameter to the page
 		self.command_label = QtWidgets.QLabel(label_parameter)
-		self.command_label.setStyleSheet("QLabel {font-size: 20px; font-weight: bold}")
+		self.command_label.setStyleSheet("""
+			QLabel {color:green; 
+			        font-size: 20px; 
+			        font-weight: bold; 
+			        margin: 20px; 
+			        padding: 5px;
+			        border: 1px solid black;
+			        border-radius: 2px
+			        }""")
 		# add label to layout
-		self.layout.addWidget(self.command_label, 0, QtCore.Qt.AlignTop) # align widget inside cell
+		self.layout.addWidget(self.command_label, 0, QtCore.Qt.AlignCenter) # align widget inside cell
 
 class CommandSection(QtWidgets.QWidget):
 	def __init__(self, what_parameter, how_parameter):
@@ -21,13 +29,23 @@ class CommandSection(QtWidgets.QWidget):
 		super(CommandSection, self).__init__()
 		# add a vertical layout
 		self.layout = QtWidgets.QVBoxLayout()
-		self.layout.setAlignment(QtCore.Qt.AlignHCenter)
+		self.layout.setAlignment(QtCore.Qt.AlignTop)
 		self.setLayout(self.layout)
 		# add the what parameter to the section as a label
 		self.what = QtWidgets.QLabel(what_parameter)
+		self.what.setFixedWidth(500)
+		self.what.setWordWrap(True)
+		self.what.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed) # no expanding in x, y
+		self.what.setStyleSheet("QLabel {font-size: 14px}")
 		# add the how parameter to the section as a text area
 		self.how = QtWidgets.QLineEdit(how_parameter)
-
+		self.how.setReadOnly(True)
+		self.how.setFixedSize(500, 30)
+		self.how.setStyleSheet("""
+			QLineEdit {font-size: 14px; 
+			           background-color: #5b7bad; 
+			           color: white; 
+			           padding-left: 5px}""")
 		# add what and how to layout
 		self.layout.addWidget(self.what, 0, QtCore.Qt.AlignCenter)
 		self.layout.addWidget(self.how, 0, QtCore.Qt.AlignCenter)
@@ -42,11 +60,15 @@ class Window(QtWidgets.QMainWindow):
 
 
 		# test: adding a page to the main window
-		self.page1 = CommandPage("Commit")
+		self.page1 = CommandPage("init")
 		self.setCentralWidget(self.page1)
 		# test: adding a section to the page
-		self.sec1 = CommandSection("Initializing a local git repository", "git init")
+		self.sec1 = CommandSection("- Initializing a local git repository in the current directory", "git init")
 		self.page1.layout.addWidget(self.sec1)
+
+		self.sec2 = CommandSection("- Add changes in the current working directory to the staging area before committing any changes.", "git add .")
+		self.page1.layout.addWidget(self.sec2)
+
 
 	#________________________________________VIEWS_______________________________________
 
@@ -56,7 +78,7 @@ class Window(QtWidgets.QMainWindow):
 
 def main():
 	app = QtWidgets.QApplication(sys.argv)
-	#app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
+	app.setStyle(QtWidgets.QStyleFactory.create("Fusion"))
 	window = Window()
 	window.show()
 	sys.exit(app.exec_())
