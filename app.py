2fec32dbb4efd38d16f39dbee56747452acaef58
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-04 14:24:35 +0200

    Add CommandPage class (for pages) and test it

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
+
+class CommandPage(QtWidgets.QWidget):
+	def __init__(self, label_parameter):
+		# initialize CommandPage as a QWidget
+		super(CommandPage, self).__init__()
+		# add a vertical layout
+		self.layout = QtWidgets.QVBoxLayout()
+		self.layout.setAlignment(QtCore.Qt.AlignHCenter) # align cells of layout
+		self.setLayout(self.layout)
+		# add a the label parameter to the page
+		self.command_label = QtWidgets.QLabel(label_parameter)
+		self.command_label.setStyleSheet("QLabel {font-size: 20px; font-weight: bold}")
+		# add label to layout
+		self.layout.addWidget(self.command_label, 0, QtCore.Qt.AlignTop) # align widget inside cell
+
 
 class Window(QtWidgets.QMainWindow):
 	def __init__(self):
@@ -8,6 +23,16 @@ class Window(QtWidgets.QMainWindow):
 		# modify Window
 		self.setWindowTitle("Git Memo")
 		self.setGeometry(500, 50, 400, 500)
+
+
+
+		# test: adding a page to the main window
+		self.page1 = CommandPage("Commit")
+		self.setCentralWidget(self.page1)
+
+	#________________________________________VIEWS_______________________________________
+
+
 
 
 
