df50fef59c506610dc9f57f485013b4dc85fb2a2
Amr-Fekry-Bosila <amrfekryy@gmail.com>
2019-01-05 20:44:09 +0200

    Create a stacked widget to contain pages and access them by command name using a dict

--- app.py
+++ app.py
@@ -10,12 +10,22 @@ class Window(QtWidgets.QMainWindow):
 		self.setWindowTitle("Git Memo")
 		self.setGeometry(500, 50, 400, 500)
 
+		# add a stacked widget for multiple pages
+		self.pages = QtWidgets.QStackedWidget()
+		# show stacked widget
+		self.setCentralWidget(self.pages)
+
 		# generate UI pages from text file attached
 		self.generator()
 
 	#_________________________________GENERATING VIEWS_______________________________________
 
 	def generator(self):
+		# a dictionary to store pages with indeces
+		page_dict = {}
+		# index counter
+		index = 0
+		# open text file and read line by line
 		with open("notes.txt", "r") as file:
 			for line in file:
 				# check if line contains a heading / what / how using regex
@@ -23,16 +33,23 @@ class Window(QtWidgets.QMainWindow):
 				what = re.search(r'- (.*?):', line)
 				how = re.search(r'  \((.*?)\)', line)
 				if heading:
-					#print(heading.group(1))
-					self.page = CommandPage(heading.group(1))
+					# store the heading (command name)
+					name = heading.group(1)
+					# create a page for the command
+					self.page = CommandPage(name)
+					# add the page to the staked widgeet "pages"
+					self.pages.addWidget(self.page)
+					# add (name: index to) the dictionary
+					page_dict[name] = index
+					index += 1
 				elif what:
 					hold_what = what
 				elif how:
-					#print(hold_what.group(1), how.group(1))
 					self.section = CommandSection(hold_what.group(1), how.group(1))
 					self.page.layout.addWidget(self.section, 0, QtCore.Qt.AlignCenter)
 
-		self.setCentralWidget(self.page)
+		# access page by command "name"
+		self.pages.setCurrentIndex(page_dict["log"])
 
 		
 
